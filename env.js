c98d520e5d63ce3bbabefd8b3b9913f5f3683e82
rendradaryono016-sudo <rendradaryono016@gmail.com>
2026-04-27 11:33:37 +0700

    Add files via upload

---
+++ env.js
@@ -0,0 +1,4 @@
+window.__SUPABASE_CONFIG__ = {
+  url: "https://tcnkwmuuyegbjbaslrax.supabase.co/rest/v1/",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjbmt3bXV1eWVnYmpiYXNscmF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNTQ2NzcsImV4cCI6MjA5MjgzMDY3N30.Yr374jIOs_8NEZTsDGGq9FP-LIQJ3IsNtk6HwWoQXXM"
+};
